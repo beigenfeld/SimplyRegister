a5f66c647310dd7c0b22def4bfe926e7890a10b2
beigenfeld <beigenfeld@gmail.com>
2017-12-04 11:46:44 -0600

    Added Company table

---
+++ SimplyRegister/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimplyRegister.Models
+{
+    public class Company
+    {
+        public string companyId { get; set; }
+
+        public string companyName { get; set; }
+        
+        public string mainContactEmail { get; set; }
+
+        public string companyMembershipLevel { get; set; }//Corporate, Assocaite, CBA, IAP, Non-Member
+    }
+}
