a9216cec674e87b7591d33ca3723332927b47f58
beigenfeld <beigenfeld@gmail.com>
2018-06-26 13:00:24 -0500

    Updated home page

--- SimplyRegister/Migrations/Configuration.cs
+++ SimplyRegister/Migrations/Configuration.cs
@@ -17,15 +17,15 @@ namespace SimplyRegister.Migrations
         protected override void Seed(SimplyRegister.Models.ApplicationDbContext context)
         {
 
-            new Company { companyName = "GC One", mainContactEmail = "help@gcone.com", companyMembershipLevel = "Corporate" };
+            new Company { companyName = "GC Co., Inc.", mainContactEmail = "help@gcone.com", companyMembershipLevel = "Corporate" };
 
-            new Company { companyName = "Associate One", mainContactEmail = "help@associateone.com", companyMembershipLevel = "Associate" };
+            new Company { companyName = "Associate Co., Inc.", mainContactEmail = "help@associateone.com", companyMembershipLevel = "Associate" };
 
-            new Company { companyName = "CBA One", mainContactEmail = "help@cbaone.com", companyMembershipLevel = "CBA" };
+            new Company { companyName = "CBA Co., Inc.", mainContactEmail = "help@cbaone.com", companyMembershipLevel = "CBA" };
 
-            new Company { companyName = "IAP One", mainContactEmail = "help@iapone.com", companyMembershipLevel = "IAP" };
+            new Company { companyName = "IAP Co., Inc.", mainContactEmail = "help@iapone.com", companyMembershipLevel = "IAP" };
 
-            new Company { companyName = "Non One", mainContactEmail = "help@nonone.com", companyMembershipLevel = "Non-Member" };
+            new Company { companyName = "NonMember Co., Inc.", mainContactEmail = "help@nonone.com", companyMembershipLevel = "Non-Member" };
             //  This method will be called after migrating to the latest version.
 
 //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
